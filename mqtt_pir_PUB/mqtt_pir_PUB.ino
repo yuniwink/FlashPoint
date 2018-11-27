464c71f02272d58b69c9a46a521f2f8d3b921d35
yuniwink <yuniwink@naver.com>
2018-11-27 21:22:00 +0900

    Create mqtt_pir_PUB.ino

---
+++ mqtt_pir_PUB/mqtt_pir_PUB.ino
@@ -0,0 +1,186 @@
+/*
+ Basic ESP8266 MQTT example
+
+ This sketch demonstrates the capabilities of the pubsub library in combination
+ with the ESP8266 board/library.
+
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic" every two seconds
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
+    else switch it off
+
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+
+ To install the ESP8266 board, (using Arduino 1.6.4+):
+  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
+       http://arduino.esp8266.com/stable/package_esp8266com_index.json
+  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
+  - Select your ESP8266 in "Tools -> Board"
+
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN            0
+#define NUMPIXELS      15
+
+int pir1 = 5;
+int pir2 = 4;
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 500; // delay for half a second
+
+int num1 = 1;
+int num2 = 2;
+int temp = 0;
+
+const char* ssid = "nintendo";
+const char* password = "switch809";
+const char* mqtt_server = "192.168.0.14";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup() {
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  pinMode(pir1, INPUT);
+  pinMode(pir2, INPUT);
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  pixels.begin();
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    Serial.println("L -> R");
+    for(int i=0;i<NUMPIXELS;i++){
+      pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(delayval); // Delay for a period of time (in milliseconds).
+      pixels.setPixelColor(i, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(50);
+    }
+  } else if ((char)payload[0] == '2') {
+    Serial.println("R -> L");
+    for(int i=14;i>=0;i--){
+      pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(delayval); // Delay for a period of time (in milliseconds).
+      pixels.setPixelColor(i, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(50);
+    }
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("1111ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("inTopic", "pir");
+      // ... and resubscribe
+      client.subscribe("aainTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  
+  int P1 = digitalRead(pir1);
+  int P2 = digitalRead(pir2);
+  if(P1 == HIGH && temp == 0) {
+    Serial.println("L -> R");
+    for(int i=0;i<NUMPIXELS;i++){
+      pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(300); // Delay for a period of time (in milliseconds).
+      pixels.setPixelColor(i, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(30);
+    }
+    snprintf (msg, 75, "%ld", num1);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("inTopic", msg);
+    temp = 1;
+  }
+  else if(P2 == HIGH && temp == 0) {
+    Serial.println("R -> L");
+    for(int i=14;i>=0;i--){
+      pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(300); // Delay for a period of time (in milliseconds).
+      pixels.setPixelColor(i, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(30);
+    }
+    snprintf (msg, 75, "%ld", num2);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("inTopic", msg);
+    temp = 2;
+  }
+}
